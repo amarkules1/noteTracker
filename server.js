afd01d5633baf1136b86d1143fb969415979733e
Alex Markules <amarlar2354@gmail.com>
2018-02-23 14:04:10 -0500

    undo port change

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const db = configureMongoose();
 const passport = configurePassport();
 const app = configureExpress(); 
 
-app.listen(80);
+app.listen(3000);
 module.exports = app;
 
 console.log('Server running at http://localhost:3000/');
